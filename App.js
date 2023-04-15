f4ed9ad8f1687ba93ec6a736508967670c523549
Lfarmer2177 <128949970+Lfarmer2177@users.noreply.github.com>
2023-04-15 17:11:37 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableOpacity, Dimensions } from 'react-native';
-// import DatePicker from 'react-native-date-picker';
-import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-native-date-picker';
+import { useState } from 'react';
 import { BleManager, Device } from 'react-native-ble-plx';
 import base64 from 'react-native-base64';
 import {LogBox} from 'react-native';
@@ -20,130 +20,285 @@ LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by mes
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 
-const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-const CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-
-
+export const manager = new BleManager();
 
 const serviceUUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 const characteristicUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 
-const manager = new BleManager();
+let test  = [0,0,0,0,0]
+let tempTemperatureArray = [0,0,0,0,0]
+let temperatureArray = [0,0,0,0,0]
 
-const BLEScreen = () => {
-  const [device, setDevice] = useState(null);
-  const [value, setValue] = useState(null);
+export default function App() {
 
-  useEffect(() => {
-    const subscription = manager.onStateChange((state) => {
-      if (state === 'PoweredOn') {
-        scanAndConnect();
-        subscription.remove();
-      }
-    }, true);
+  //const [temperatureArray, setTemperatureArray] = useState([])
 
-    return () => {
-      manager.destroy();
-    };
-  }, []);
+  const [temperatureArrayLength, setTemperatureArrayLength] = useState(0); 
 
-  useEffect(() => {
-  setValue(value);
-}, [value]);
+    //Is a device connected?
+    const [isConnected, setIsConnected] = useState(false);
+
+    //What device is connected?
+    const [connectedDevice, setConnectedDevice] = useState();
+
+    const [message, setMessage] = useState(0);
+
+    //let [tempTemperatureArray, setTempTemperatureArray] = useState([0,0,0,0,0])
+
+    
+ 
+
+    
+    
+
 
   const scanAndConnect = () => {
-    manager.startDeviceScan(null, null, (error, scannedDevice) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
+    console.log("I am Pushed")
+    //const manager = new BleManager();
+    manager.startDeviceScan(null, null, (error, device) => {
+      console.log("starting Scan")
+        if (error) {
+            // Handle error (scanning will be stopped automatically)
+            console.log("There is some error")
+            //console.log(error)
+            //console.log(device)
+            console.log(JSON.stringify(error));
+            //manager.stopDeviceScan();
+            return
+        }
 
-      if (scannedDevice.name === 'My BLE Server') {
-        console.log('Found device:', scannedDevice.name);
-        manager.stopDeviceScan();
-        connectToDevice(scannedDevice);
-      }
+        // Check if it is a device you are looking for based on advertisement data
+        // or other criteria.
+        if (device.name === 'TI BLE Sensor Tag' || 
+            device.name === 'My BLE Server') {
+            
+            // Stop scanning as it's not necessary if you are scanning for one device.
+            manager.stopDeviceScan();
+            console.log("Successflly cooneced")
+            connectDevice(device)
+
+        }
+
     });
-  };
 
-  const connectToDevice = (scannedDevice) => {
-    console.log('Connecting to device:', scannedDevice.name);
-    scannedDevice.connect()
-      .then((connectedDevice) => {
-        console.log('Connected to device:', connectedDevice.name);
-        setDevice(connectedDevice);
-        setupNotifications(connectedDevice);
-        return device.discoverAllServicesAndCharacteristics();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+    setTimeout(() => {
+    manager.stopDeviceScan();
+    }, 10000);
 
-  const setupNotifications = (connectedDevice) => {
-    console.log('Setting up notifications');
-    connectedDevice.discoverAllServicesAndCharacteristics()
-      device.readCharacteristicForService(serviceUUID, characteristicUUID)
+  }
+
+    // handle the device disconnection (poorly)
+    async function disconnectDevice() {
+      console.log('Disconnecting start');
+  
+      if (connectedDevice != null) {
+        const isDeviceConnected = await connectedDevice.isConnected();
+        if (isDeviceConnected) {
+          manager.cancelTransaction('messagetransaction');
+          manager.cancelTransaction('nightmodetransaction');
+  
+          manager.cancelDeviceConnection(connectedDevice.id).then(() =>
+            console.log('DC completed'),
+          );
+        }
+  
+        const connectionStatus = await connectedDevice.isConnected();
+        if (!connectionStatus) {
+          setIsConnected(false);
+        }
+      }
+    }
+
+    //Connect the device and start monitoring characteristics
+    async function connectDevice(device) {
+      console.log('connecting to Device:', device.name);
+  
+      device
+        .connect()
+        .then(device => {
+          setConnectedDevice(device);
+          setIsConnected(true);
+          return device.discoverAllServicesAndCharacteristics();
+        })
+        .then(device => {
+          //  Set what to do when DC is detected
+          manager.onDeviceDisconnected(device.id, (error, device) => {
+            console.log(device.name, ' is DC');
+            setIsConnected(false);
+          });
+  
+          //Read inital values
+  
+          //Message
+          device
+            .readCharacteristicForService(serviceUUID, characteristicUUID)
             .then(valenc => {
-              //console.log(valenc);
-              //console.log(base64.decode(valenc.value))
-              setValue(base64.decode(valenc.value));
-            })
-      .then((characteristics) => {
-        const characteristic = characteristics.find((c) => c.uuid === CHARACTERISTIC_UUID);
-        return characteristic.monitor((error, characteristic) => {
-          if (error) {
-            console.error(error);
-            return;
-          }
+              console.log(valenc);
+              console.log(base64.decode(valenc.value))
+              setMessage(base64.decode(valenc.value));
+              temperatureArray.push(base64.decode(valenc.value))
+            });
 
-           if (characteristic.value != null) {
-                  
-                const setValue = base64.decode(characteristic.value);
-                setValue(value);
-           }
+          //monitor values and tell what to do when receiving an update
+          //Message
+          device.monitorCharacteristicForService(
+            serviceUUID,
+            characteristicUUID,
+            (error, characteristic) => {
+              if(error){
+                console.log(error)
+              }
+              else if (characteristic?.value != null) {
+                setMessage(base64.decode(characteristic.value));
+                temperatureArray.push(base64.decode(characteristic.value));
+                const now =new Date;
+                console.log(
+                  'Message update received:',
+                  base64.decode(characteristic.value),
+                  now.getHours(),":",now.getMinutes(),":",now.getSeconds().toString().padStart(2,'0')
+                );
+                //console.log("length of temperature array is: ", temperatureArray.length);
+                if (temperatureArray.length % 5 === 0) {
+                  //console.log("temperature array full, values are:", temperatureArray)
+                  //console.log("last 5 elements are", temperatureArray.slice(Math.max(temperatureArray.length - 5, 0)))
+                  test = temperatureArray.slice(Math.max(temperatureArray.length - 5, 0))
+                  tempTemperatureArray = test;
+                  //console.log("Plot values are", test, tempTemperatureArray)
+                  //temperatureArray = [];
+                }
 
-          
-          
-        });
 
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const disconnect = () => {
-    console.log('Disconnecting from device');
-    device.cancelConnection()
-      .then(() => {
-        console.log('Disconnected from device');
-        setDevice(null);
-        setValue(null);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  // const handleChange = (event) => {
-  //   setValue(event.target.value);
-  // };
+              }
+              else {
+                console.log("I dont know why I am here")
+              }
+            },
+            'messagetransaction',
+          );
+          console.log('Connection established');
+        });   
+    }
 
   return (
     <View style={styles.container}>
-      {device ? (
-        <>
-          <Text>Connected to {device.name}</Text>
-          <Text>Value: {value}</Text>
-          <Button title="Disconnect" onPress={disconnect} />
-        </>
-      ) : (
-        <Button title="Connect" onPress={scanAndConnect} />
-      )}
+      <Text>{message}</Text>
+      <Text>Array Length is now: {temperatureArray.length}</Text>
+      
+
+      <TouchableOpacity style={styles.button}>
+          {!isConnected ? (
+            <Button
+              title="Connect"
+              onPress={() => {
+                scanAndConnect();
+              }}
+              disabled={false}
+            />
+          ) : (
+            <Button
+              title="Disonnect"
+              onPress={() => {
+                disconnectDevice();
+              }}
+              disabled={false}
+            />
+          )}
+        </TouchableOpacity>
+        
+        
+
+        <Text>Line Chart</Text>
+
+        <View>
+  
+  <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May"],
+      datasets: [
+        {
+          data: temperatureArray.length % 5 === 0 ? test : tempTemperatureArray //test //temperatureArray.slice(Math.max(temperatureArray.length - 5, 0))
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  /> 
+  </View>
+
+  <Text>Line Chart</Text>
+        <View>
+        <ProgressChart
+        data={[message/100]}
+        width={ Dimensions.get("window").width }
+        height={220}
+        radius={50}
+        chartConfig={{
+          //backgroundColor: '#478438',
+          backgroundGradientFrom: '#FFF8E1',
+          backgroundGradientTo: '#FFF8E1',
+          //decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`,
+        }}
+        style= {{
+          borderRadius: 15,
+        }}
+        hideLegend={true}
+      />
+        {/* <ProgressChart
+  data= {0.4}
+  width={Dimensions.get("window").width}
+  height={220}
+  strokeWidth={16}
+  radius={32}
+  chartConfig={{
+          //backgroundColor: '#478438',
+          backgroundGradientFrom: '#FFF8E1',
+          backgroundGradientTo: '#FFF8E1',
+          //decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`,
+        }}
+//   chartConfig={{
+//   backgroundGradientFrom: "#1E2923",
+//   backgroundGradientFromOpacity: 0,
+//   backgroundGradientTo: "#08130D",
+//   backgroundGradientToOpacity: 0.5,
+//   color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+//   strokeWidth: 2, // optional, default 3
+//   barRadius: 0.5
+// }}
+  hideLegend={false}
+/> */}
+        </View>
+
+
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -159,6 +314,3 @@ const styles = StyleSheet.create({
     width: 120,
   },
 });
-
-
-export default BLEScreen;
